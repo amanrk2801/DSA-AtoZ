9f851227173904a5dacddd78ec138e0e50b41ffc
amanrk2801 <amankumbhalwar09@gmail.com>
2024-07-11 12:22:26 +0530

    config

--- first.cpp
+++ first.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 int main()
 {
-    cout<<"Hello, World"<<endl;
+    int x;
+    cin>>x;
+    cout<<"Hello, World "<<x<<endl;
+    for(int i = 0; i<10; i++){
+        cout<<i<<endl;
+    }
     return 0;
 }
 
